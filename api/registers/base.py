6c65b8ead55550fa9eb698c58197a365ad80d0b3
roodrigo <francisco@miflink.com>
2022-01-21 12:48:17 -0600

    Change settings

--- api/registers/base.py
+++ api/registers/base.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!^2klo2ahf#9dlu!msw-odecngh-phti2vc($&3=(ci4o=eixa'
+SECRET_KEY = env.str("SECRET_KEY", '123qwe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
