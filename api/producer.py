7507ea52dbfe4568bae887f1d8f0a00cc9f72541
roodrigo <francisco@miflink.com>
2022-01-21 12:47:52 -0600

    Feat: Added broker

--- api/producer.py
+++ api/producer.py
@@ -1,9 +1,7 @@
-from calendar import c
 import queue
-
-from django.db import connection
 import pika, json
 from abc import ABC, abstractmethod
+from django.conf import settings
 
 class Broker(ABC):
 
@@ -24,21 +22,30 @@ class BrokerCredentials(Broker):
     user: str
     password: str
     queue: str
+    host: str
     port: int
     vhost: str
 
 
 class RabbitMQBroker(BrokerCredentials):
 
+    user: str = settings.USER_RABBITMQ
+    password: str = settings.PASSWORD_RABBITMQ
+    queue: str = settings.QUEUE_RABBITMQ
+    vhost: str = settings.RABBITMQ_VHOST
+    port: int = settings.RABBITMQ_PORT
+    host: str = settings.RABBITMQ_HOST
+
+
     def connection(self):
-        credentials = pika.PlainCredentials('guest', 'guest')
-        parameters = pika.ConnectionParameters('broker',
-                                                5672,
-                                                '/',
+        credentials = pika.PlainCredentials(self.user, self.password)
+        parameters = pika.ConnectionParameters(self.host,
+                                                self.port,
+                                                self.vhost,
                                                 credentials)
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue="notifications", durable=True, exclusive=False, auto_delete=False)
+        self.channel.queue_declare(queue=self.queue, durable=True, exclusive=False, auto_delete=False)
         self.channel.confirm_delivery()
 
     def connection_is_alive(self) -> bool:
@@ -50,11 +57,10 @@ class RabbitMQBroker(BrokerCredentials):
             if not self.connection_is_alive or self.connection:
                 self.connection()
                 self.channel.basic_publish(exchange='',
-                                    routing_key='notifications',
+                                    routing_key=self.queue,
                                     body=json.dumps(body),
                                     properties=pika.BasicProperties(content_type='application/json',
                                                                     delivery_mode=1))
-                print('Message publish was confirmed')
             else:
                 self.connection()
                 self.publish(body)
